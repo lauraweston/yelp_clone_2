422d7c28f95b272071dc15687cce1fb90c50ad60
Laura Weston <laura.weston@outlook.com>
2016-11-18 15:25:36 +0000

    One user can only write one review per restaurant

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ActiveRecord::Base
   has_many :reviews
   has_many :restaurants
   has_many :reviewed_restaurants, through: :reviews, source: :restaurant
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
@@ -25,6 +26,9 @@ class User < ActiveRecord::Base
       end
     end
   end
-
+  # 
+  # def has_reviewed?(restaurant)
+  #   reviewed_restaurants.include?(restaurant)
+  # end
 
 end
